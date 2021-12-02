6e2febeba9f7ef75d683cc4cafd579b6fcb3da6f
HritikNegi <91362469+HritikNegi@users.noreply.github.com>
2021-12-02 10:18:09 +0530

    Create task3.cpp

---
+++ practical8/task3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){ 
+
+    int a = 20 ;
+    
+    //  V1.
+    
+    int *p ;
+    p = &a ;
+    
+    int **q = &p ;
+    
+    cout << *p << endl ;
+    cout << **q << endl ;
+
+    //  V2.
+
+    **q = 10 ;
+    
+    cout << a << endl ;
+
+    return 0 ;
+}
